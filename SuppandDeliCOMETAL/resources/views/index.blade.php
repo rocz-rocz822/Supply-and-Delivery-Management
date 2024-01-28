be8f4d16a2107f238d9ba77425ab07deba3cff28
june-milan <justinemaem03+github@gmail.com>
2024-01-28 22:52:50 +0800

    edited

--- SuppandDeliCOMETAL/resources/views/index.blade.php
+++ SuppandDeliCOMETAL/resources/views/index.blade.php
@@ -16,6 +16,13 @@
   <!-- Admin Forms CSS -->
   <link rel="stylesheet" type="text/css" href="{{ asset('admin/admin-tools/admin-forms/css/admin-forms.css') }}">
 
+  <!-- Select2 Plugin CSS  -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendor/plugins/select2/css/core.css')}}">
+
+  <!-- plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+  <!-- End plugin css for this page -->
+
   <!-- Theme CSS -->
   <link rel="stylesheet" type="text/css" href="{{ asset('admin/skin/default_skin/css/theme.css')}}">
 
@@ -69,9 +76,52 @@
 
   <!-- BEGIN: PAGE SCRIPTS -->
 
+  <!-- plugins:js -->
+  <script src="{{ asset('admin/vendors/base/vendor.bundle.base.js') }}"></script>
+  <!-- endinject -->
+
+  <!-- Plugin js for this page-->
+  <script src="{{ asset('admin/vendors/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+  <script src="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+  <!-- End plugin js for this page-->
+
+  <!-- inject:js -->
+  <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
+  <script src="{{ asset('admin/js/hoverable-collapse.js') }}"></script>
+  <script src="{{ asset('admin/js/template.js') }}"></script>
+  <!-- endinject -->
+
+  <!-- Custom js for this page-->
+  <script src="{{ asset('admin/js/dashboard.js') }}"></script>
+  <script src="{{ asset('admin/js/data-table.js') }}"></script>
+  <script src="{{ asset('admin/js/jquery.dataTables.js') }}"></script>
+  <script src="{{ asset('admin/js/dataTables.bootstrap4.js') }}"></script>
+  <!-- End custom js for this page-->
+
+  <script src="{{ asset('admin/js/jquery.cookie.js') }}" type="text/javascript"></script>
+
   <!-- jQuery -->
   <script src="{{ asset('vendor/jquery/jquery-1.11.1.min.js')}}"></script>
   <script src="{{ asset('vendor/jquery/jquery_ui/jquery-ui.min.js')}}"></script>
+
+  <!-- Select2 Plugin Plugin -->
+  <script src="{{ asset('vendor/plugins/select2/select2.min.js')}}"></script>
+
+  <script src="{{ asset('vendor/jquery/jquery-1.11.1.min.js')}}"></script>
+  <script src="{{ asset('vendor/jquery/jquery_ui/jquery-ui.min.js')}}"></script>
+
+  <!-- Datatables -->
+  <script src="{{ asset('vendor/plugins/datatables/media/js/jquery.dataTables.js')}}"></script>
+
+  <!-- Datatables Tabletools addon -->
+  <script src="{{ asset('vendor/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js')}}"></script>
+
+  <!-- Datatables ColReorder addon -->
+  <script src="{{ asset('vendor/plugins/datatables/extensions/ColReorder/js/dataTables.colReorder.min.js')}}"></script>
+
+  <!-- Datatables Bootstrap Modifications  -->
+  <script src="{{ asset('vendor/plugins/datatables/extensions/ColReorder/js/dataTables.colReorder.min.js')}}"></script>
 
   <!-- Admin Forms JS -->
   <script src="{{ asset('admin/admin-tools/admin-forms/js/jquery-ui-datepicker.min.js')}}"></script>
@@ -118,6 +168,29 @@
         if (!smartpikr.hasClass(themeClass)) {
           inst.dpDiv.wrap('<div class="' + themeClass + '"></div>');
         }
+      }
+    });
+
+    // Init Boostrap Multiselects
+    $('#multiselect1').multiselect();
+
+    $('#multiselect2').multiselect({
+      buttonClass: 'multiselect dropdown-toggle btn btn-default btn-primary'
+    });
+
+    "use strict";
+
+    // Init Theme Core
+    Core.init();
+
+    // Init Demo JS
+    Demo.init();
+
+    // Init DataTables
+    $('#datatable').dataTable({
+      "sDom": '',
+      "oTableTools": {
+        "sSwfPath": "vendor/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
       }
     });
 
